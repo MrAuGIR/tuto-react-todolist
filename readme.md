6f50d2dd52851c87d38f51545eca5465a86389da
MrAuGIR <augirard17@gmail.com>
2022-02-27 22:58:55 +0100

    tsx config

--- readme.md
+++ readme.md
@@ -11,4 +11,15 @@ package.json
     "build": "cross-env NODE_ENV=production webpack"
   },
 ```
-creation a le rcine du projet de webpack.config.js
+creation a le racine du projet de webpack.config.js
+
+creation du fichier tsconfig.json
+
+mise en place de react
+`` npm i react react-dom ``
+ajouter les types  (typescript react ?)
+`` npm i -D @types/react @types/react-dom ``
+
+loader
+`` npm install tslint tslint-loader tslint-config-standard ``
+creation du fichier tslint.json a la racine
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,39 @@
 const path = require('path')
 
+const webpack = require('webpack')
+
 let config = {
-    entry: './src/main.tsx'
+    entry: './src/main.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: '/dist/'
+    },
+    resolve: {
+        extensions: ['.js','.ts','.tsx']
+    },
+    module:{
+        rules: [
+            {
+                test: /\.tsx?/,
+                loader: 'tslint-loader',
+                enforce: 'pre',
+                exclude: [/node_modules/]
+            },
+            {
+                test: /\.tsx?/,
+                loader: 'ts-loader',
+                exclude: [/node_modules/]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'proces.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
+        })
+    ]
 }
+
+module.exports = config
